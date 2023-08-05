01f21194bf0ba8b403f5c440a668756be8d9c094
FIONA18 <32681518+FIONA18@users.noreply.github.com>
2023-08-05 21:48:02 +0530

    feat: Implemented function to Convert date to String
    ConvertDateToString() converts the date object to another object whose day, Month, year fields are of string

--- palindrome.js
+++ palindrome.js
@@ -7,4 +7,42 @@ function isPalindrome(str)
 {
     return str === reverseString(str);
 }
-console.log(isPalindrome('dood'))
+
+function ConvertDateToString(date)
+{
+    var dateString =
+    {
+        day:'',
+        month:'',
+        year:''
+    }
+    if (date.day < 10)
+    {
+        dateString.day = '0' + date.day;
+    }
+    else
+    {
+        dateString.day =  date.day.toString();
+    }
+
+    if (date.month < 10)
+    {
+        dateString.month = '0' + date.month;
+    }
+    else
+    {
+        dateString.month =  date.month.toString();
+    }
+    dateString.year = date.year.toString();
+    return dateString;
+    
+}
+
+var date = 
+{
+    day:5,
+    month:6,
+    year:2000
+}
+
+console.log(ConvertDateToString(date))
